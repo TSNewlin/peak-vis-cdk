171cbadaae98971d8cb1736d82a7397643d81038
Tyler Newlin <tnewlin25@gmail.com>
2022-11-27 15:24:58 -0500

    Initial commit

---
+++ test/peak-vis-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PeakVisCdk from '../lib/peak-vis-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/peak-vis-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PeakVisCdk.PeakVisCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
