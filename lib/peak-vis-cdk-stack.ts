149e20ac72ed0616dcce030b29c7c74a3f1266ea
Tyler Newlin <tnewlin25@gmail.com>
2022-11-27 21:26:52 -0500

    Add s3 bucket and iam users with permissions to stack

--- lib/peak-vis-cdk-stack.ts
+++ lib/peak-vis-cdk-stack.ts
@@ -1,8 +1,45 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { User, AccessKey } from 'aws-cdk-lib/aws-iam'
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 
 export class PeakVisCdkStack extends cdk.Stack {
+  readonly bucket: Bucket;
+  readonly iamUploadUser: User;
+  readonly iamGetUser: User;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    this.bucket = new Bucket(this, "Omnicept-Data-Bucket", {
+      bucketName: "omnicept-data-bucket",
+    });
+
+    this.iamUploadUser = new User(this, "Omnicept-Data-Bucket-Write-User", {userName: "OmniceptDataUploadUser"});
+    this.iamGetUser = new User(this, "Omnicept-Data-Bucket-Read-User", {userName: "OmniceptDataViewUser"});
+    this.provisionAccessKeys();
+    this.configureBucketPolicies();
+  }
+
+  private provisionAccessKeys() {
+    const uploadUserAccessKey = new AccessKey(this, "Omnicept-Data-Bucket-Write-User-Key", {
+      user: this.iamUploadUser
+    });
+    const getUserAccessKey = new AccessKey(this, "Omnicept-Data-Bucket-Read-User-Key", {
+      user: this.iamGetUser
+    });
+    new Secret(this, "Omnicept-Data-Bucket-Read-User-Secret", {
+      secretStringValue: getUserAccessKey.secretAccessKey
+    });
+    new Secret(this, "Omnicept-Data-Bucket-Write-User-Secret", {
+      secretStringValue: uploadUserAccessKey.secretAccessKey,
+    });
+  }
+
+  private configureBucketPolicies() {
+    this.bucket.grantPut(this.iamUploadUser);
+    this.bucket.grantWrite(this.iamUploadUser);
+    this.bucket.grantRead(this.iamGetUser);
   }
 }
--- test/peak-vis-cdk.test.ts
+++ test/peak-vis-cdk.test.ts
@@ -1,17 +1,25 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as PeakVisCdk from '../lib/peak-vis-cdk-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as PeakVisCdk from '../lib/peak-vis-cdk-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/peak-vis-cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new PeakVisCdk.PeakVisCdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+const app = new cdk.App();
+const stack = new PeakVisCdk.PeakVisCdkStack(app, 'TestStack');
+const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test('S3 bucket created', () => {
+  template.hasResourceProperties('AWS::S3::Bucket', {
+    BucketName: "omnicept-data-bucket"
+  });
+}); 
+
+test('IAM user for bucket uploads created', () => {
+    template.hasResourceProperties('AWS::IAM::User', {
+        UserName: "OmniceptDataUploadUser"
+    });
+});
+
+test('IAM user for reading s3 data created', () => {
+    template.hasResourceProperties('AWS::IAM::User', {
+        UserName: "OmniceptDataViewUser"
+    })
 });
