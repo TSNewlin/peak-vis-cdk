899bd4792593a603021ab64030826a7e417bd4a5
Tyler Newlin <tsnewlin@bsu.edu>
2022-12-06 11:15:08 -0500

    Add test for userId params that have non-digits

--- test/peak-vis-lambda.test.js
+++ test/peak-vis-lambda.test.js
@@ -1,16 +1,26 @@
 const list = require("../lib/lambda/list");
 
-test("Function rejects useId not of length 6", async () => {
+const invalidUserIdResponse = {
+    body: JSON.stringify({
+        message: "Invalid userId in path parameter"
+    }),
+    statusCode: 400
+}
+
+test("Function to list uploaded datasets rejects userId not of length 6", async () => {
     const event = {
         pathParameters: {
             userId: "1234"
         }
     };
-    const errorResponse = {
-        body: JSON.stringify({
-            message: "Invalid userId in path parameter"
-        }),
-        statusCode: 400
-    };
-    expect(await list.main(event)).toEqual(errorResponse)
+    expect(await list.main(event)).toEqual(invalidUserIdResponse)
+});
+
+test("Function to list uploaded datasets rejects userId that contains non-digit characters", async () => {
+    const event = {
+        pathParameters: {
+            userId: "12as56"
+        }
+    }
+    expect(await list.main(event)).toEqual(invalidUserIdResponse)
 });
